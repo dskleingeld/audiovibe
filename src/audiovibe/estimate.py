db26fe57f202bdbba7c873ca08a31924fbdaab6b
dskleingeld <11743287+dskleingeld@users.noreply.github.com>
2021-07-06 18:45:16 +0200

    adds trained weights/model and database of annotations

--- src/audiovibe/estimate.py
+++ src/audiovibe/estimate.py
@@ -4,8 +4,8 @@ from audiovibe.think.predict import predict, load_model
 
 if __name__ == "__main__":
 
-    # path = "test/Beethoven - Moonlight Sonata (1st Movement)_test.m4a"
-    path = "test/vivaldi_storm.m4a"
+    path = "test/Beethoven - Moonlight Sonata (1st Movement)_test.m4a"
+    # path = "test/vivaldi_storm.m4a"
     # path = "test/relaxed45_test.m4a"
     samples = Samples.from_path(path)
     features = Features.from_samples(samples)
